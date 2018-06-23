7e506116dffac8afd4d343cf565176682a97753a
VitorSerio <vitorbserio@gmail.com>
2018-06-23 04:06:31 -0300

    graficos pro modelo

--- EP2/test.py
+++ EP2/test.py
@@ -40,6 +40,13 @@ def derivate(x, y, y0 = 0):
         
     return yi
 
+def frange(start, stop, step=1.0):
+    
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
 ##################################
 #            DADOS               #
 ##################################
@@ -81,7 +88,7 @@ l = 0.823                                # comprimento das cordas               
 a = 0.051                               # largura do cesto                           (m)
 b = 0.144                               # comprimento do cesto                       (m)
 L = math.sqrt(l**2 - (a/2)**2 - (b/2)**2)  # comprimento da corda imaginária do pêndulo (m)
-mp = 0.2                                # massa do pendulo                           (kg) - CHUTE!!
+mp = 0.23                                # massa do pendulo                          (kg)
 Ap = 0.0059                             # área frontal do pêndulo                    (m^2)
 p = []                                  # lista para armazenar os dados do giroscópio
 tmin_p = [7.05, 5.15, 5.2, 5.15, 5.3]   # tempos iniciais (s)
@@ -178,7 +185,7 @@ plt.show()
 
 ############### MCU ###############
 
-plt.figure(figsize = (25, 10), facecolor = '#FFFFFF')
+plt.figure(figsize = (25, 12), facecolor = '#FFFFFF')
 plt.suptitle('Valores experimentais para os experimentos de Movimento Circular Uniforme')
 for i in range(5):
     plt.subplot(3, 5, i+1)
@@ -227,15 +234,106 @@ for i in range(1,5):
     re = re.append(r[i], ignore_index = True)
     ce = ce.append(c[i], ignore_index = True)
     pe = pe.append(p[i], ignore_index = True)
+    
+dt = 0.001
 
 ########## Bloco em rampa #########
 
 Br = 0.2 * par * Ar / mr
 Ar = g * math.sin(thetar)
 
+tmax = max(re['time'])
+    
+eur1 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'd' : 0.0,
+                    'v' : 0.0,
+                    'a' : 0.0})
+
+eur1['d'][0] = 0.0
+eur1['v'][0] = 0.0
+eur1['a'][0] = Ar
+    
+for i in range(1, len(eur1)):
+    eur1['d'][i] = eur1['d'][i-1] + eur1['v'][i-1] * dt
+    eur1['v'][i] = eur1['v'][i-1] + eur1['a'][i-1] * dt
+    eur1['a'][i] = Ar - Br * eur1['v'][i] ** 2
+
+eur2 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'd' : 0.0,
+                    'v' : 0.0,
+                    'a' : 0.0})
+
+eur2['d'][0] = 0.0
+eur2['v'][0] = 0.0
+eur2['a'][0] = Ar
+    
+for i in range(1, len(eur2)):
+    eur2['a'][i] = Ar - Br * eur2['v'][i-1] ** 2
+    eur2['v'][i] = eur2['v'][i-1] + eur2['a'][i] * dt
+    eur2['d'][i] = eur2['d'][i-1] + eur2['v'][i] * dt
+    
+e1 = []
+e2 = []
+
+for i in range(len(re)):
+    e1.append(re['d'][i] - eur1['d'][math.floor(re['time'][i] * 1000)])
+    e2.append(re['d'][i] - eur2['d'][math.floor(re['time'][i] * 1000)])
+    
+re['d_e1'] = e1
+re['d_e2'] = e2
+
+e1 = []
+e2 = []
+
+for i in range(len(re)):
+    e1.append(re['v'][i] - eur1['v'][math.floor(re['time'][i] * 1000)])
+    e2.append(re['v'][i] - eur2['v'][math.floor(re['time'][i] * 1000)])
+    
+re['v_e1'] = e1
+re['v_e2'] = e2
+
+e1 = []
+e2 = []
+
+for i in range(len(re)):
+    e1.append(re['gFx'][i] - eur1['a'][math.floor(re['time'][i] * 1000)])
+    e2.append(re['gFx'][i] - eur2['a'][math.floor(re['time'][i] * 1000)])
+    
+re['a_e1'] = e1
+re['a_e2'] = e2
+
+
 ############### MCU ###############
 
 wc = np.mean(ce['w'])
+
+tmax = max(ce['time'])
+    
+euc1 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'theta' : 0.0})
+
+euc1['theta'][0] = 0.0
+    
+for i in range(1, len(euc1)):
+    euc1['theta'][i] = euc1['theta'][i-1] + wc * dt
+
+euc2 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'theta' : 0.0})
+
+euc2['theta'][0] = 0.0
+    
+for i in range(1, len(euc2)):
+    euc2['theta'][i] = euc2['theta'][i-1] + wc * dt
+    
+e1 = []
+e2 = []
+
+for i in range(len(ce)):
+    e1.append(ce['theta'][i] - euc1['theta'][math.floor(ce['time'][i] * 1000)])
+    e2.append(ce['theta'][i] - euc2['theta'][math.floor(ce['time'][i] * 1000)])
+    
+ce['theta_e1'] = e1
+ce['theta_e2'] = e2
 
 ############# Pêndulo #############
 
@@ -243,3 +341,196 @@ Bp = 0.2 * par * Ap * L / mp
 thetap0 = np.mean(theta0)
 Ap = g / L
 
+tmax = max(pe['time'])
+
+eup1 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'theta' : 0.0,
+                    'w' : 0.0,
+                    'a' : 0.0})
+
+eup1['theta'][0] = -thetap0
+eup1['w'][0] = 0.0
+eup1['a'][0] = Ap * math.sin(thetap0)
+    
+for i in range(1, len(eup1)):
+    eup1['theta'][i] = eup1['theta'][i-1] + eup1['w'][i-1] * dt
+    eup1['w'][i] = eup1['w'][i-1] + eup1['a'][i-1] * dt
+    eup1['a'][i] = -Ap * math.sin(eup1['theta'][i]) - Bp * eup1['w'][i] * abs(eup1['w'][i])
+
+eup2 = pd.DataFrame({'time' : list(frange(0.0, tmax, dt)),
+                    'theta' : 0.0,
+                    'w' : 0.0,
+                    'a' : 0.0})
+
+eup2['theta'][0] = -thetap0
+eup2['w'][0] = 0.0
+eup2['a'][0] = Ap * math.sin(thetap0)
+    
+for i in range(1, len(eup2)):
+    eup2['a'][i] = -Ap * math.sin(eup2['theta'][i-1]) - Bp * eup2['w'][i] * abs(eup2['w'][i-1])
+    eup2['w'][i] = eup2['w'][i-1] + eup2['a'][i] * dt
+    eup2['theta'][i] = eup2['theta'][i-1] + eup2['w'][i] * dt
+    
+e1 = []
+e2 = []
+
+for i in range(len(pe)):
+    e1.append(pe['theta'][i] - eup1['theta'][math.floor(pe['time'][i] * 1000)])
+    e2.append(pe['theta'][i] - eup2['theta'][math.floor(pe['time'][i] * 1000)])
+    
+pe['theta_e1'] = e1
+pe['theta_e2'] = e2
+
+e1 = []
+e2 = []
+
+for i in range(len(pe)):
+    e1.append(pe['wx'][i] - eup1['w'][math.floor(pe['time'][i] * 1000)])
+    e2.append(pe['wx'][i] - eup2['w'][math.floor(pe['time'][i] * 1000)])
+    
+pe['w_e1'] = e1
+pe['w_e2'] = e2
+
+e1 = []
+e2 = []
+
+for i in range(len(pe)):
+    e1.append(pe['a'][i] - eup1['a'][math.floor(pe['time'][i] * 1000)])
+    e2.append(pe['a'][i] - eup2['a'][math.floor(pe['time'][i] * 1000)])
+    
+pe['a_e1'] = e1
+pe['a_e2'] = e2
+    
+    
+##################################
+#            GRAFICOS            #
+##################################
+
+########## Bloco em rampa #########
+
+tmax = max(re['time'])
+
+plt.figure(figsize = (15, 10), facecolor = '#FFFFFF')
+plt.suptitle('Resultado do modelo com seus respectivos erros para o experimento do Bloco em Rampa')
+
+plt.subplot(2, 3, 1)
+plt.title('Aceleração')
+plt.scatter(re['time'], re['gFx'], s = 1)
+plt.plot(eur1['time'], eur1['a'], color = 'C2')
+plt.plot(eur2['time'], eur2['a'], color = 'C1')
+plt.ylabel(r'Aceleração (m/s$^2$)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 2)
+plt.title('Velocidade')
+plt.scatter(re['time'], re['v'], s = 1)
+plt.plot(eur1['time'], eur1['v'], color = 'C2')
+plt.plot(eur2['time'], eur2['v'], color = 'C1')
+plt.ylabel('Velocidade (m/s)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 3)
+plt.title('Distância')
+plt.scatter(re['time'], re['d'], s = 1)
+plt.plot(eur1['time'], eur1['d'], color = 'C2')
+plt.plot(eur2['time'], eur2['d'], color = 'C1')
+plt.ylabel('Distância (m)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 4)
+plt.scatter(re['time'], re['a_e1'], s = 1)
+plt.scatter(re['time'], re['a_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel(r'Erro (m/s$^2$)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 5)
+plt.scatter(re['time'], re['v_e1'], s = 1)
+plt.scatter(re['time'], re['v_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel('Erro (m/s)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 6)
+plt.scatter(re['time'], re['d_e1'], s = 1)
+plt.scatter(re['time'], re['d_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel('Erro (m)')
+plt.xlabel('Tempo (s)')
+plt.show()
+
+############### MCU ###############
+
+tmax = max(ce['time'])
+
+plt.figure(figsize = (10, 5), facecolor = '#FFFFFF')
+plt.suptitle('Resultado do modelo com seus respectivos erros para o experimento do Movimento Circular Uniforme')
+
+plt.subplot(1, 2, 1)
+plt.scatter(ce['time'], ce['theta'], s = 1)
+plt.plot(euc1['time'], euc1['theta'], color = 'C2')
+plt.plot(euc2['time'], euc2['theta'], color = 'C1')
+plt.ylabel('Distância angular (m)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(1, 2, 2)
+plt.scatter(ce['time'], ce['theta_e1'], s = 10)
+plt.scatter(ce['time'], ce['theta_e2'], s = 10, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel('Erro (m)')
+plt.xlabel('Tempo (s)')
+plt.show()
+
+############# Pêndulo #############  
+
+tmax = max(pe['time'])
+
+plt.figure(figsize = (15, 10), facecolor = '#FFFFFF')
+plt.suptitle('Resultado do modelo com seus respectivos erros para o experimento do Movimento Pendular')
+
+plt.subplot(2, 3, 1)
+plt.title('Aceleração Angular') 
+plt.scatter(pe['time'], pe['a'], s = 1)
+plt.plot(eup1['time'], eup1['a'], color = 'C2')
+plt.plot(eup2['time'], eup2['a'], color = 'C1')
+plt.ylabel(r'Aceleração angular (rad/s$^2$)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 2)
+plt.title('Velocidade Angular')
+plt.scatter(pe['time'], pe['wx'], s = 1)
+plt.plot(eup1['time'], eup1['w'], color = 'C2')
+#plt.plot(eup2['time'], eup2['w'], color = 'C1')
+plt.ylabel('Velocidade angular (rad/s)')
+plt.xlabel('Tempo (s)')
+plt.show()
+
+plt.subplot(2, 3, 3)
+plt.title('Inclinação')
+plt.scatter(pe['time'], pe['theta'], s = 1)
+plt.plot(eup1['time'], eup1['theta'], color = 'C2')
+plt.plot(eup2['time'], eup2['theta'], color = 'C1')
+plt.ylabel('Inclinação (rad)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 4)
+plt.scatter(pe['time'], pe['a_e1'], s = 1)
+plt.scatter(pe['time'], pe['a_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel(r'Erro (rad/s$^2$)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 5)
+plt.scatter(pe['time'], pe['w_e1'], s = 1)
+plt.scatter(pe['time'], pe['w_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel('Erro (rad/s)')
+plt.xlabel('Tempo (s)')
+
+plt.subplot(2, 3, 6)
+plt.scatter(pe['time'], pe['theta_e1'], s = 1)
+plt.scatter(pe['time'], pe['theta_e2'], s = 1, color = 'C1')
+plt.plot([0, tmax], [0, 0], color = 'C2')
+plt.ylabel('Erro (rad)')
+plt.xlabel('Tempo (s)')
+plt.show()
